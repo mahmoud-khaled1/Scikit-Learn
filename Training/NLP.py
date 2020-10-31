c61c7f272a9feaf0286486f1392a3e09d9eebd78
mahmoud-khaled1 <mahmoudkhaled01020@gmail.com>
2020-10-31 21:25:56 +0200

    add

---
+++ Training/NLP.py
@@ -0,0 +1,8 @@
+#   Natural Language Processing (NLP)
+#	تختص بشكل أساسي في التعامل مع النصوص و ما يسمي تحليل الإنطباع Sentiment Analysis
+#	     يتم استخدامها عبر اثنين من الموديلوز في sklearn  وهي :
+#	     preprocessing.LabelEncoder				لتحويل النصوص في الفيتشرز الي ارقام
+#	     preprocessing.OneHotEncoder			لصناعة مصفوفة الواحد من النصوص
+#	     feature_extraction.text.CountVectorizer		لقراءة النصوص الطويلة و معالجتها
+
+#1-LabelEncoder : used in ML to convert test in column to number in  DataSets
