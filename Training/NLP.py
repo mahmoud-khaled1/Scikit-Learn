b00d52cbdb490340980debba317694a8eba7e05e
mahmoud-khaled1 <mahmoudkhaled01020@gmail.com>
2020-10-31 22:25:08 +0200

    add

--- Training/NLP.py
+++ Training/NLP.py
@@ -51,5 +51,27 @@ df.drop('score',axis='columns',inplace=True)
 #Print dataFrame
 print('Updates dataframe is : \n', df)
 
+#-----------
+#another Example
+from sklearn.preprocessing import LabelEncoder
+import pandas as pd
+
+data = pd.read_csv(r'D:\ML\SKLearn Library\Slides && Data\Data\2.16 NLP\mall.csv')
+#print(data.head())
+df = pd.DataFrame(data)
+print('Original dataframe is : \n' ,df )
+enc  = LabelEncoder()
+enc.fit(df['Genre'])
+print('classed found : ' , list(enc.classes_))
+
+print('equivilant numbers are : ' ,enc.transform(df['Genre']) )
+
+df['Genre Code'] = enc.transform(df['Genre'])
+print('Updates dataframe is : \n' ,df )
+
+print('Inverse Transform  : ' ,list(enc.inverse_transform([1,0,1,1,0,0])))
+
+#---------------------------------------------------
+#2-OneHotEncoder
 
 
