faa3d7cc4f3d6c6ca44d13c8e223d5ff22ec05c6
mahmoud-khaled1 <mahmoudkhaled01020@gmail.com>
2020-10-31 22:19:26 +0200

    add

--- Training/NLP.py
+++ Training/NLP.py
@@ -5,4 +5,51 @@
 #	     preprocessing.OneHotEncoder			لصناعة مصفوفة الواحد من النصوص
 #	     feature_extraction.text.CountVectorizer		لقراءة النصوص الطويلة و معالجتها
 
-#1-LabelEncoder : used in ML to convert test in column to number in  DataSets
+#1-LabelEncoder : used in ML to convert test in column to numbers in  DataSets
+from sklearn.preprocessing import LabelEncoder
+#ٍSteps of algorithm
+"""
+
+#Create object from class LabelEncoder
+LE=LabelEncoder()
+#Fit to specific column
+LE.fit(dataset['NameOfColumn'])
+#Transform it
+LE.transform(dataset['NameOfColumn'])
+#update Column
+dataset['NewColumn']=LE.transform(dataset['NameOfColumn'])
+
+"""
+#Example
+
+from sklearn.preprocessing import LabelEncoder
+import pandas as pd
+
+raw_data = {'patient': [1, 1, 1, 2, 2],
+            'obs': [1, 2, 3, 1, 2],
+            'treatment': [0, 1, 0, 1, 0],
+            'score': ['strong', 'weak', 'normal', 'weak', 'strong']}
+df = pd.DataFrame(raw_data, columns=['patient', 'obs', 'treatment', 'score'])
+
+print('Original dataframe is : \n', df)
+
+# Create a label (category) encoder object
+le = LabelEncoder()
+# Fit the encoder to the pandas column
+le.fit(df['score'])
+#Print what is text that sound to be replaced
+print('classed found : ', list(le.classes_))
+
+# column data after replace text with numbers
+print('equivilant numbers are : ', le.transform(df['score']))
+#add new column with numbers instead of text
+df['newscore'] = le.transform(df['score'])
+
+#Drop old column Score
+df.drop('score',axis='columns',inplace=True)
+
+#Print dataFrame
+print('Updates dataframe is : \n', df)
+
+
+
